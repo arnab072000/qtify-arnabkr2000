3c391163bd23d57a1c5f094f171fb08f8777e7e1
arnab072000 <arnab11kr@gmail.com>
2023-11-14 00:19:58 +0530

    "logo css commit"

--- src/components/Logo/Logo.jsx
+++ src/components/Logo/Logo.jsx
@@ -1,10 +1,11 @@
 import React from "react";
+import "./Logo.css";
 import LogoImage from "../../assects/Group 1.png";
 
 function Logo() {
     return (
         
-        <img src= {LogoImage} alt="Logo" width={67} />
+        <img src= {LogoImage} alt="Logo" className="logo" />
         
     )
 }
